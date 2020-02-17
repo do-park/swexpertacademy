c105ee8464ed3311011db6921bf5aa8448798c1e
do-park <dohee.pa@gmail.com>
2020-02-17 17:23:47 +0900

    1/1 done

---
+++ 00_200217/1224_계산기3.py
@@ -0,0 +1,31 @@
+def parse_expr(expStr):
+    tokens = tokenize(expStr)
+    OP = ("*", "/", "+", "-", "(", ")")
+    P = {
+        "*" : 50,
+        "/" : 50,
+        "+" : 40,
+        "-" : 40,
+        "(" : 0
+    }
+    output = []
+    stack = []
+
+    for item in tokens:
+        if item not in OP:
+            output.append(item)
+        elif item == "(":
+            stack.append(item)
+        elif item == ")":
+            while stack != [] and stack[-1] != "(":
+                output.append(stack.pop())
+            stack.pop()
+        else:
+            while stack != [] and P[stack[-1]] >= P[item]:
+                output.append(stack.pop())
+            stack.append(item)
+
+    while stack:
+        output.append(stack.pop())
+
+    return output
