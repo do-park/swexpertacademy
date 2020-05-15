0ae48e8f444f9e55fd326cf85597ee943405cb4e
박도희 <dohee.pa@gmail.com>
2020-05-15 13:18:56 +0900

    May 15. Done

--- learn/04. Programming Advanced/05_백트래킹/5209.py
+++ learn/04. Programming Advanced/05_백트래킹/5209.py
@@ -1,19 +1,22 @@
 # SWEA 5209 최소 생산 비용
-# Fail (Runtime error), 5/10 pass
 
-from itertools import permutations
+def dfs(now, result):
+    global answer
+    if now == N:
+        answer = min(answer, result)
+        return
+    if answer < result:
+        return
+    for n in range(N):
+        if not visited[n]:
+            visited[n] = True
+            dfs(now + 1, result + maps[now][n])
+            visited[n] = False
 
 for tc in range(1, int(input()) + 1):
     N = int(input())
     maps = [list(map(int, input().split())) for _ in range(N)]
-    P = list(permutations([n for n in range(N)]))
+    visited = [False] * N
     answer = 1500
-    for p in P:
-        result = 0
-        for n in range(N):
-            result += maps[n][p[n]]
-            if result > answer:
-                break
-        else:
-            answer = min(answer, result)
+    dfs(0, 0)
     print(f'#{tc} {answer}')
